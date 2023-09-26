25f92a6f1ebc4cf8840fd688b555a39681d277dc
David Hunter <codeclanstudent@codeclans-mbp.home>
2023-09-26 22:23:32 +0100

    week 9 day 2 update

--- src/components/About.js
+++ src/components/About.js
@@ -4,14 +4,35 @@ import { styled } from 'styled-components';
 const AboutDiv = styled.div`
 display: flex;
 justify-content: space-around;
-background-color: darkorange;
+background: rgb(255,255,255);
+background: linear-gradient(0deg, rgba(255,255,255,1) 0%, rgba(254,145,2,1) 76%, rgba(235,124,4,1) 100%);
 height: auto;
 padding: 0 0 0 5px;
 gap: 5px;
+
+@media(max-width: 768px) {
+    flex-direction: column;
+    justify-content: center;
+    margin: auto;
+    padding: 0 0 5px 5px;
+}
 `
 
 const Title = styled.h1`
 text-align: center;
+`
+
+const AboutSec = styled.section`
+border-top: 1px solid black;
+`
+
+const AboutImg = styled.img`
+    width: 350px;
+    object-fit: cover;
+    border: 3px solid black;
+    line-height: 0;
+    margin: 50px 0 50px 0;
+    box-sizing: border-box;
 `
 
 const About = () => {
@@ -20,25 +41,25 @@ const About = () => {
         <article>
         <Title> About me </Title>
 
-        <section>
+        <AboutSec>
         <h3> Coding Journey </h3>
         <p> In June 2023 I joined CodeClan's Professional Software Development course where I found my passion for coding.</p>  
         <p> The liquidation of CodeClan during my second week of the course only fueled my determination to carry on, thankfully I was able to continue my studies under CodeBase and have to date completed 8 of the 16 weeks.  </p>
         <p> During this time I have built several full stack apps using Python, Javascript, React, HTML, CSS and PostGreSQL. Some examples of my work can be found under the projects tab.  </p>
-        </section>
-        <section>
+        </AboutSec>
+        <AboutSec>
         <h3> Past experience</h3>
         <p> Prior to venturing into software development I was an Electrical Engineer within a consultancy focusing on building services.</p>
         <p> My day-to-day here including crafting designs for a range of electrical services including lighting & fire alarms, collaborating with other teams within and external to my company and dealing with technical queries</p>
-        </section>
-    <section>
+        </AboutSec>
+        <AboutSec>
         <h3> Personal Life</h3>
         <p> Outside of work and coding I am an active member of Glasgow's Dodgeball team, <a href = 'https://www.instagram.com/kelvinrapids/?hl=en'>Kelvin Rapids</a>, regularly representing the team at leagues and tournaments and for which I am an event's prganiser and the team's Wellness Officer.</p>
         <p>I also have had a lifelong obsession with gaming and have in the past, livestreamed on Twitch and written for gaming website, GodisaGeek. You can see some of my favourite games here. </p>
-        </section>
+        </AboutSec>
         </article>
 
-        <img src = {require("../images/me.jpg")} alt ='a handsome bespectacled man in a blue suit with a floral orange tie'/>
+        <AboutImg src = {require("../images/me.jpg")} alt ='a handsome bespectacled man in a blue suit with a floral orange tie'/>
         
         </AboutDiv>
      );
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -10,55 +10,63 @@ padding: 0 0 0 5px;
 gap: 5px;
 `
 
+
 const Title = styled.h1`
 text-align: center;
+`
 
+const Project = styled.section`
+display: flex;
+border-top: 2px solid black;
+gap: 10px;
+`
+
+const ProjectAlt = styled.section`
+display: flex;
+flex-direction: row-reverse;
+border-top: 2px solid black;
+gap: 10px;
+`
+
+const Desc = styled.p`
+margin: 2% 0 2% 0;
+`
+
+const ProjectImg = styled.img`
+    width: 350px;
+    object-fit: cover;
+    border: 3px solid black;
+    line-height: 0;
+    margin: 50px 0 50px 0;
+    box-sizing: border-box;
 `
 
 const Projects = () => {
     return ( 
         <ProjectsDiv>
         <article>
-        <Title> Some of my work </Title>
+        <Title> Stuff Dave Made </Title>
         
 
-        <section>
+        <Project>
+        <div>
         <h3> Sickley's Veterinary Surgery </h3>
-        <p> Objective: To create an application for managing a veterinary practice focusing on CRUD functionality for vet staff, pets and owners.</p>  
+        <p> Objective: To create an application to be useful for managing a veterinary practice, providing full CRUD functionality to the user for practice staff as well as registered pets and owners.</p>  
         <p> Tech Stack: Python backend, HTML & CSS frontend with Flask framework, PostgreSQL for database management. </p>
-        <p> Key Learnings:</p>
-        <ul>
-            <li>
-                Time required to follow methodical process for planning, testing, writing code and debugging.
-            </li>
-            <li>
-                Consolidation of several technologies into my first full-stack application.
-            </li>
-            <li>
-                Advancement of CSS knowledge.
-            </li>
-        </ul>  
-        <img src =''></img>
-        </section>
+        <Desc> Sickley's was my first proper full stack application as well as my first module-capping project during CodeClan. This week long task taught me how vital planning is when it comes to coding as well as how time consuming it can be to properly test and debug along the way. While my understanding of all the technologies improved a great deal, my CSS abilities were seriously lacking going into this project and so that stands out as a key lesson. Overall I'm very proud of this work and showed it off to many friends and family.</Desc>
+        </div>
+        <ProjectImg src ={require("../images/vetmanagementapp.png")} alt = 'screenshot of vet management app home page'/>
+        </Project>
 
-        <section>
-        <h3> Pokemon  </h3>
-        <p> Objective: To create an application for managing a veterinary practice focusing on CRUD functionality for vet staff, pets and owners.</p>  
-        <p> Tech Stack: Python backend, HTML & CSS frontend with Flask framework, PostgreSQL for database management. </p>
-        <p> Key Learnings:</p>
-        <ul>
-            <li>
-                Time required to follow methodical process for planning, testing, writing code and debugging.
-            </li>
-            <li>
-                Consolidation of several technologies into my first full-stack application.
-            </li>
-            <li>
-                Advancement of CSS knowledge.
-            </li>
-        </ul>  
-        <img src =''></img>
-        </section>
+        <ProjectAlt>
+        <div>
+        <h3> Pokemon Selector </h3>
+        <p> Objective: To handle JSON information from an API, convert it to JavaScript and display to the user in a readable format.</p>  
+        <p> Tech Stack: Javascript with React. </p>
+        <Desc> As a lifelong gamer, creating a Pokedex application spoke to me when the time came to attempt implementing an API into a React application. The particular challenge here was the list of Pokemon from PokeAPI presented only a name and a URL, necessitating a second fetch request to obtain further information from the user selection.</Desc>
+        </div>
+        <ProjectImg src ={require("../images/vetmanagementapp.png")} alt = 'screenshot of vet management app home page'/>
+        </ProjectAlt> 
 
 
 
